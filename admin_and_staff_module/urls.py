729a18ec6b46e174446bb71e86fcaf8dd295509b
awazone <awazoneinfo@gmail.com>
2024-03-27 23:16:38 +0900

    third commit

--- admin_and_staff_module/urls.py
+++ admin_and_staff_module/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-# from .views import SlidePhotoListCreateAPIView,SlidePhotoRetrieveUpdateDestroyAPIView
 
 urlpatterns=[
     path('dashboard/',views.admin_dashboard,name='admin_dashboard'),
@@ -11,6 +10,4 @@ urlpatterns=[
     path('manage/terms_and_conditions/',views.manage_terms_and_conditions,name='manage_terms_and_conditions'),
     path('levels',views.admin_view_level,name='admin_view_level'),
     path('add_levels',views.add_level,name='add_level'),
-    # path('api/slide-photos/', SlidePhotoListCreateAPIView.as_view(), name='slide-photo-list-create'),
-    # path('api/slide-photos/<int:pk>/', SlidePhotoRetrieveUpdateDestroyAPIView.as_view(), name='slide-photo-detail'),
 ]
